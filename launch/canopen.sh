9213d361688e6a2626aee5fd8e24c61fb77f4e98
06liukai <1102106545@qq.com>
2017-06-27 01:11:12 +0900

    ros_canusb_package

--- launch/canopen.sh
+++ launch/canopen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# Bind the USBCAN device
-sudo -S slcand -o -c -f -s6 /dev/ttyUSB0 slcan0
-echo "slcand ttyUSB0 slcan0 "
+# Bind the USBCAN device /dev/ttyUSB1
+sudo -S slcand -o -c -f -s6 tty_usb slcan0
+echo "slcand ttyusb slcan0 "
 sleep 0.5
 sudo -S ifconfig slcan0 up
 echo "ifconfig slcan0 up "
---
+++ launch/tty.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# put rules file into etc
+sudo cp 90-ttyusb.rules /etc/udev/rules.d
+echo "copy down."
+sleep 0.5
